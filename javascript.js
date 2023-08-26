d46d7400666714046db6eb223088946303136734
DigvijayRajebhosale9697 <139614453+DigvijayRajebhosale9697@users.noreply.github.com>
2023-08-26 17:34:08 +0530

    Commit-3

--- javascript.js
+++ javascript.js
@@ -7,3 +7,15 @@ window.addEventListener('load', function () {
   const container = document.querySelector('.container');
   container.classList.add('show');
 });
+
+const Accordians=document.querySelectorAll(".Accordian");
+
+Accordians.forEach(Accordian =>{
+    const icon = Accordian.querySelector(".icon");
+    const Answer=Accordian.querySelector(".Answer");
+
+    Accordian.addEventListener("click", ()=>{
+        icon.classList.toggle("Active");
+        Answer.classList.toggle("Active");
+    })
+})
